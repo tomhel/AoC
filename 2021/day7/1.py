de81bcc2bb2f46966e7fccf2336f0b321124a722
Tommy Hellström <tommy.hellstrom@lm.se>
2021-12-07 12:24:45 +0100

    day 7, 2021

---
+++ 2021/day7/1.py
@@ -0,0 +1,17 @@
+def load():
+    with open("input") as f:
+        return [int(x) for x in f.read().strip().split(",")]
+
+
+def align_crabs():
+    crabs = load()
+    min_fuel = None
+
+    for pos in range(min(crabs), max(crabs) + 1):
+        fuel = sum(abs(c - pos) for c in crabs)
+        min_fuel = fuel if min_fuel is None else min(min_fuel, fuel)
+
+    return min_fuel
+
+
+print(align_crabs())
