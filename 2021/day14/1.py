d4b47a66a8e215ec3ea063664e8c87e8258e9c1b
Tommy Hellström <tommy.hellstrom@lm.se>
2021-12-14 13:29:43 +0100

    day 14, part 1, 2021

---
+++ 2021/day14/1.py
@@ -0,0 +1,32 @@
+def load():
+    with open("input") as f:
+        yield next(f).strip()
+        next(f)
+        for x in f:
+            a, b = x.strip().split(" -> ")
+            yield a, b
+
+
+def pair_insertion():
+    data = list(load())
+    polymer, rules = list(data[0]), dict(data[1:])
+
+    for _ in range(10):
+        new_polymer = [polymer[0]]
+
+        for i in range(len(polymer) - 1):
+            pair = polymer[i] + polymer[i + 1]
+            new_polymer.extend((rules[pair], polymer[i + 1]))
+
+        polymer = new_polymer
+
+    histogram = {}
+
+    for e in polymer:
+        histogram[e] = histogram.get(e, 0) + 1
+
+    count = sorted(histogram.values())
+    return count[-1] - count[0]
+
+
+print(pair_insertion())
---
+++ 2021/day14/README.md
@@ -0,0 +1,4 @@
+# Day 14: Extended Polymerization
+
+https://adventofcode.com/2021/day/14
+
