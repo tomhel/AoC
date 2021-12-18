b470de81157e4257319595989b2c0d0892b28a81
Tommy Hellström <tommy.hellstrom@lm.se>
2021-12-18 02:27:23 +0100

    day 17, 2021

---
+++ 2021/day17/1.py
@@ -0,0 +1,53 @@
+import math
+
+
+def load():
+    with open("input") as f:
+        _, _, xrange, yrange = next(f).strip().split(" ")
+        x1, x2 = xrange[2:][:-1].split("..")
+        yield int(x1), int(x2)
+        y1, y2 = yrange[2:].split("..")
+        yield int(y1), int(y2)
+
+
+def is_target_hit(x, y, target_x, target_y):
+    x1, x2 = target_x
+    y1, y2 = target_y
+    return x1 <= x <= x2 and y1 <= y <= y2
+
+
+def is_target_miss(x, y, target_x, target_y):
+    x1, x2 = target_x
+    y1, y2 = target_y
+    return x > x2 or y < y1
+
+
+def launch_probe(dx, dy, target_x, target_y):
+    max_height = -math.inf
+    x, y = 0, 0
+
+    while True:
+        if is_target_miss(x, y, target_x, target_y):
+            return -math.inf
+        elif is_target_hit(x, y, target_x, target_y):
+            return max_height
+
+        x, y = x + dx, y + dy
+        max_height = max(max_height, y)
+        dx = dx - 1 if dx > 0 else dx
+        dx = dx + 1 if dx < 0 else dx
+        dy -= 1
+
+
+def find_highest_position():
+    target_x, target_y = list(load())
+    max_height = -math.inf
+
+    for dx in range(1, target_x[1] + 1):
+        for dy in range(-abs(target_y[0]), abs(target_y[0])):
+            max_height = max(max_height, launch_probe(dx, dy, target_x, target_y))
+
+    return max_height
+
+
+print(find_highest_position())
---
+++ 2021/day17/README.md
@@ -0,0 +1,4 @@
+# Day 17: Trick Shot
+
+https://adventofcode.com/2021/day/17
+
