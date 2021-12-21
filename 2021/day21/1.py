bd2cd330f35717d86e2692ca133ddf0d98fdf101
Tommy Hellström <tommy.hellstrom@lm.se>
2021-12-21 07:07:25 +0100

    day 21, part 1, 2021

---
+++ 2021/day21/1.py
@@ -0,0 +1,23 @@
+def load():
+    with open("input") as f:
+        for x in f:
+            yield int(x.split(":")[1].strip())
+
+
+def play():
+    players = list(load())
+    score = [0, 0]
+    die = 0
+
+    while True:
+        for p in range(len(players)):
+            moves = sum(x % 100 + 1 for x in range(die, die + 3))
+            die += 3
+            pos = (players[p] + moves - 1) % 10 + 1
+            players[p] = pos
+            score[p] += pos
+            if score[p] >= 1000:
+                return min(score) * die
+
+
+print(play())
---
+++ 2021/day21/README.md
@@ -0,0 +1,4 @@
+# Day 21: Dirac Dice
+
+https://adventofcode.com/2021/day/21
+
