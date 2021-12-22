ba2d23d8704cef32d402889ac488b28a0586659e
Tommy Hellström <tommy.hellstrom@lm.se>
2021-12-22 03:58:34 +0100

    day 21, part 2, 2021

---
+++ 2021/day21/2.py
@@ -0,0 +1,29 @@
+def load():
+    with open("input") as f:
+        for x in f:
+            yield int(x.split(":")[1].strip())
+
+
+def roll(players, p, score, wins, universes):
+    for die, count in [(3, 1), (4, 3), (5, 6), (6, 7), (7, 6), (8, 3), (9, 1)]:
+        score2 = list(score)
+        players2 = list(players)
+        pos = (players2[p] + die - 1) % 10 + 1
+        players2[p] = pos
+        score2[p] += pos
+
+        if score2[p] >= 21:
+            wins[p] += universes * count
+        else:
+            roll(players2, (p + 1) % 2, score2, wins, universes * count)
+
+    return wins
+
+
+def play():
+    players = list(load())
+    return max(roll(players, 0, [0] * len(players), [0] * len(players), 1))
+
+
+# Takes about 1 minute to run...
+print(play())
