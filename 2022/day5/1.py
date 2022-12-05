5efd77a9f00a5cb6d5065704b09fdccf181a41e4
Tommy Hellström <tommy.hellstrom@lm.se>
2022-12-05 11:24:48 +0100

    2022 day5

---
+++ 2022/day5/1.py
@@ -0,0 +1,31 @@
+def load():
+    with open("input") as f:
+        stacks = {}
+        for line in f:
+            if line.strip() == "":
+                break
+            for i in range(0, len(line), 4):
+                if line[i] == "[":
+                    crates = stacks.get(i // 4, [])
+                    crates.append(line[i + 1])
+                    stacks[i // 4] = crates
+        yield {k: list(reversed(v)) for k, v in stacks.items()}
+        for line in f:
+            _, cnt, _, frm, _, to = line.strip().split(" ")
+            yield int(cnt), int(frm) - 1, int(to) - 1
+
+
+def move(cnt, frm, to, stacks):
+    crates = [stacks[frm].pop() for _ in range(cnt)]
+    stacks[to].extend(crates)
+
+
+def rearrange():
+    data = load()
+    stacks = next(data)
+    for cnt, frm, to in data:
+        move(cnt, frm, to, stacks)
+    return "".join(stacks[i][-1] for i in sorted(stacks))
+
+
+print(rearrange())
---
+++ 2022/day5/README.md
@@ -0,0 +1,4 @@
+# Day 5: Supply Stacks
+
+https://adventofcode.com/2022/day/5
+
